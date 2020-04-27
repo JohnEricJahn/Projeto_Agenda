8604775b089467767a2d3754b74fe43b463e4339
FrostPriest <johnericjahn@gmail.com>
2020-04-26 23:47:38 -0300

    Cadastrando Contatos

--- routes.js
+++ routes.js
@@ -2,15 +2,21 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+const { loginRequired } = require('./src/middlewares/middleware');
 
 // Rotas De Home
 route.get('/', homeController.index);
 
 // Rotas De Login
 route.get('/login/index', loginController.index);
-// Criando rota de registro -- Chamando método do controller.
 route.post('/login/register', loginController.register);
-// Criando rota de login -- Chamando método do controller.
 route.post('/login/login', loginController.login);
-route.get('/    login/logout', loginController.logout);
+route.get('/login/logout', loginController.logout);
+
+// Rotas de contato
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
+
 module.exports = route;
